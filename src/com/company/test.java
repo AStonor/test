a8ffa554c054a3a5def93d8b2321da2e357da407
Andreas Kristensen <lampefeber@hotmail.com>
2021-08-07 12:52:27 +0200

    test

---
+++ src/com/company/test.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class test {
+
+    public static void test1(){
+        System.out.println("test 1");
+    }
+
+    public static void test2(){
+        System.out.println("test 2");
+    }
+
+    public static void test3(){
+        System.out.println("test 3");
+    }
+
+}
